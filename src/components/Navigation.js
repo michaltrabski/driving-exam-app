93784d1dbec2318eab7adc1b4e23a18fdb011f9b
Michal Trabski <michal.trabski@gmail.com>
2019-10-23 08:53:04 +0200

    Navigation converted to react-bootstrap menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,71 +1,32 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import styled, { css } from "styled-components";
 import { path } from "./../data/GlobalData";
-
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.dark};
-  color: ${({ theme }) => theme.light};
-`;
-const Logo = styled.div`
-  display: flex;
-  flex-grow: 1;
-
-  height: 100px;
-  align-items: center;
-  a {
-    text-decoration: none;
-    color: ${({ theme }) => theme.light};
-  }
-`;
-const Ul = styled.ul`
-  /* background: lightyellow; */
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  flex-grow: 10;
-  list-style-type: none;
-`;
-const Li = styled.li`
-  margin-left: 4rem;
-  list-style-type: none;
-  text-align: center;
-  a {
-    text-decoration: none;
-    color: ${({ theme }) => theme.light};
-
-    &.active {
-      color: ${({ theme }) => theme.white};
-      font-weight: ${({ theme }) => theme.weight_bold};
-    }
-  }
-`;
+import { Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
   return (
-    <Nav>
-      <Logo>
-        <h3>
-          <Link to="/">poznajTesty.pl</Link>
-        </h3>
-      </Logo>
-      <Ul>
-        <Li>
-          <NavLink to={path.learn}>Nauka Pytań </NavLink>
-        </Li>
-        <Li>
-          <NavLink to={path.exam}>Wykonaj Egzamin </NavLink>
-        </Li>
-        <Li>
-          <NavLink to={path.exam_reviev}>Przeglądaj wyniki egzaminów</NavLink>
-        </Li>
-        <Li>
-          <NavLink to={path.blog}>Blog</NavLink>
-        </Li>
-      </Ul>
-    </Nav>
+    <Navbar bg="dark" variant="dark" expand="lg" className="shadow">
+      <Link className="navbar-brand" to="/">
+        poznajTesty.pl
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink className="nav-link" to={path.learn}>
+            Nauka Pytań
+          </NavLink>
+          <NavLink className="nav-link" to={path.exam}>
+            Wykonaj Egzamin
+          </NavLink>
+          <NavLink className="nav-link" to={path.exam_reviev}>
+            Przeglądaj wyniki egzaminów
+          </NavLink>
+          <NavLink className="nav-link" to={path.blog}>
+            Blog
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
