827a1f29bb684ed3039715867f6609ece3dc7e93
Michal Trabski <michal.trabski@gmail.com>
2019-11-15 09:54:09 +0100

    added UserProfile

--- src/config/path.js
+++ src/config/path.js
@@ -6,7 +6,8 @@ const path = {
   stats: "/statystyki",
   super_admin: "/super-admin",
   sign_in: "/logowanie",
-  sign_up: "/rejestracja"
+  sign_up: "/rejestracja",
+  user_profile: "/twoj-profil"
 };
 
 export default path;
