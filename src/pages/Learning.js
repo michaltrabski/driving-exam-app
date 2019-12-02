d2552c7cdee69213709666792479bd990da1371d
Michal Trabski <michal.trabski@gmail.com>
2019-12-02 09:20:22 +0100

    search questions fixed

--- src/pages/Learning.js
+++ src/pages/Learning.js
@@ -7,12 +7,11 @@ import SearchForm from "../components/learning/SearchForm";
 import { Container, Row, Col } from "../elements/elements";
 import Filters from "./../components/learning/Filters";
 import SearchInfo from "../components/learning/SearchInfo";
-import { filterQuestions, SHOW_ALL, SHOW_BAD } from "../functions/functions";
+import { filterQuestions, SHOW_ALL } from "../functions/functions";
 import Add from "../components/Add";
 import Loading from "../components/learning/Loading";
 import FilteresOutInfo from "../components/learning/FilteredOutInfo";
 import { GetQuestions } from "../functions/functionalComponents";
-import { storage } from "./../functions/functions";
 
 const Learning = () => {
   let { allQuestions, cqi, perPage, search, filterOption } = useSelector(
@@ -20,18 +19,18 @@ const Learning = () => {
   );
 
   // filter array based on search result
-  allQuestions = allQuestions.filter(question =>
-    question.t.includes(search.toLowerCase())
-  );
+  allQuestions = allQuestions.filter(question => {
+    let q = question.t.toLowerCase();
+    let s = search.toLowerCase();
+    return q.indexOf(s) === -1 ? false : true;
+  });
 
   // filter array based on filter that user choose
   if (filterOption !== SHOW_ALL) {
     allQuestions = allQuestions.filter(question =>
       filterQuestions(question, filterOption)
     );
-    // if (filterOption === SHOW_BAD && !storage("t")) storage("t", allQuestions);
   }
-  // if (storage("t")) allQuestions = storage("t");
 
   let amount = allQuestions.length;
 
